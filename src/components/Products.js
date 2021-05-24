ef1020803abfe9e4b70b58d3cf8ee960a3ec6744
steveldev <contact@reseau-net.fr>
2021-05-24 02:15:22 +0200

    feature : item delete

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,29 +3,30 @@ import React, { useState } from 'react';
 const Products = () => {
     
     let productsData = [
-        {name: 'produit1', price: 50},
-        {name: 'produit2', price: 150},
-        {name: 'produit3', price: 250},
+        {id: 1, name: 'produit1', price: 50},
+        {id: 2, name: 'produit2', price: 150},
+        {id: 3, name: 'produit3', price: 250},
     ];
     
     const [items, setItems] = useState(productsData);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        let form = document.querySelector('.ProductForm form');
-        if(form) {
-            // convert formdata to object
-            let item = new FormData(form);
-            item = Object.fromEntries(item.entries());
-
-            // update items content
-            setItems([...items,item]);              
-        }
-    };
-
+  
     const ProductForm = () => {
 
+      const handleSubmit = (e) => {
+            e.preventDefault();
+
+            let form = document.querySelector('.ProductForm form');
+            if(form) {
+                // convert formdata to object
+                let item = new FormData(form);
+                item = Object.fromEntries(item.entries());
+                item.id = items.length +1;
+                
+                // update items content
+                setItems([...items,item]);              
+            }
+        };
         return (
             <div className="ProductForm">            
                 <h2>Gestion des produits</h2>
@@ -62,10 +63,28 @@ const Products = () => {
     };
 
     const ProductItem = ({item}) => {
+
+        const handleDelete = (e) => {
+            const productItems = [];
+            items.forEach(item => {
+                if(item.id != e.target.id) {
+                    productItems.push(item)
+                }
+            })
+            
+            setItems(productItems);
+        }
+
         return (
             <li className="d-flex justify-content-between">
                 <span>{item.name}</span>        
-                <span>{item.price} &euro;</span>
+                <span>{item.price} &euro;</span>                
+                <a  href="#" title="supprimer ce produit" class="btn"
+                    id={item.id}
+                    onClick={ e =>
+                        window.confirm('Confirmer la supression') && handleDelete(e)
+                    }
+                >&times;</a>
             </li>
         );
     };
